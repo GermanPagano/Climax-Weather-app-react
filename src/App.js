f890d262641cfbd16d10abdb7c038c28b8f47ac9
germanpagano <sergiogermanpagano@gmail.com>
2022-12-14 19:12:37 -0300

    changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+
+  // estados para la busqueda , el icono y el valor
+  const [ search , setSearch ] = useState('');
+  const [ icon, setIcon ] = useState('');
+  const [ value , setValue] = useState('');
+
+
+  // useEffect ejecutara la funcion getData cuando existan cambios en el estado
+  useEffect(() => {
+    getData()
+  }, [ ])
+
+
+// funcion getData para buscar datos de la api
+  const URL =' https://api.openweathermap.org/data/2.5/weather?q=roma&appid=3d7b683a89edb0c24fd7cd7102449ccc'
+  const getData = async() => {
+    console.log(' Enter the Api ')
+      await fetch (URL)
+      .then ( Response => Response.json())
+      .then ( data =>  console.log(data.weather[0].main))
+      .catch( err => {
+        console.log(err)
+      })
+  }
+
+  
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        hola soy una app react
+
       </header>
+      Climax by German pagano - All rights reserved
     </div>
   );
 }
